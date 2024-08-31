140250072a0d3a8b592730b1e26556fbfb126aa6
NikhilJade <jadenik13@gmail.com>
2024-08-31 15:32:11 +0530

    feat | change forms format

--- thumbnail/forms.py
+++ thumbnail/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Video
 
 class VideoForm(forms.ModelForm):
+    video_path = forms.CharField(label='Video URL or Local Path', widget=forms.TextInput(attrs={'placeholder': 'Enter URL or local path'}))
+    
     class Meta:
         model = Video
-        fields = ['video_url']
+        fields = ['video_path']
